45038c45c042c447808daba40b0fc78b7d0598f5
code0o0 <w17074919615@gmail.com>
2024-01-21 11:26:10 +0800

    test

--- bot/modules/bot_settings.py
+++ bot/modules/bot_settings.py
@@ -119,7 +119,7 @@ async def get_buttons(key=None, edit_type=None):
                 value = "[...]"
             elif k == "USER_SESSION_STRING":
                 value = value[:5] + "..." + value[-5:]
-            msg += f'<pre>{index+1}. {k} = {value}</pre\n'
+            msg += f'<pre>{index+1}. {k} = {value}</pre>\n'
             buttons.ibutton(index+1, f"botset botvar {k}", position="header")
         pages = (len(var_list) - 1) // 14 + 1
         if START == 0:
