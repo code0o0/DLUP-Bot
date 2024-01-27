29e8361bef9988a19fd0ec29c7fe9f1cc959b7ad
code0o0 <w17074919615@gmail.com>
2024-01-27 10:33:32 +0800

    Fix bug

--- bot/modules/edit_media.py
+++ bot/modules/edit_media.py
@@ -16,7 +16,6 @@ async def edit_media(client, message):
     text = message.text.split(' ', 1)[1] if len(message.text.split()) > 1 else ''
     command = message.command
     origin_message = message.reply_to_message
-    forward_chat = origin_message.forward_from_chat
     if not origin_message or not origin_message.media or origin_message.poll:
         msg = 'Please use <i>/cmd caption</i> or <i>/cmd</i> to reply to the media message you want to edit!\n'
         msg += '<i>/cmd caption -i</i> to ignore the source of the message.'
@@ -24,19 +23,15 @@ async def edit_media(client, message):
         reply_message = await sendMessage(message, msg)
         await auto_delete_message(message, reply_message, delay=20)
         return
+    forward_chat = origin_message.forward_from_chat
     protect_content = True if '-p' in command else False
     ignore_source = True if '-i' in command else False
     caption = text.rsplit('-i', 1)[-1].rsplit('-p', 1)[-1].strip()    
     try:
-        
-        
         if forward_chat and not ignore_source:
             forward_chat_username = forward_chat.username
             forward_from_message_id = origin_message.forward_from_message_id
             caption += f'\nSOURCE: <b><a href="https://t.me/{forward_chat_username}/{forward_from_message_id}">{forward_chat_username}</a></b>'
-        
-        
-        
         if origin_message.media_group_id:
             media_group = await client.get_media_group(chat_id, origin_message.id)
             send_medias = []
@@ -58,7 +53,8 @@ async def edit_media(client, message):
                 send_medias[0].caption = caption
             send_medias[0].parse_mode = ParseMode.HTML
             await client.send_media_group(chat_id, send_medias, protect_content=protect_content)
-            await client.delete_messages(chat_id, [msg.id for msg in media_group].append(message.id))
+            msg_list = [msg.id for msg in media_group] + [message.id]
+            await client.delete_messages(chat_id, msg_list)
         else:
             if not caption or caption.startswith('\nSOURCE:'):
                 media_caption = origin_message.caption.html if origin_message.caption else ''
