3d35862e949e98eaf377959b9a13847e8ac41c5f
code0o0 <w17074919615@gmail.com>
2024-01-27 10:42:30 +0800

    update

--- bot/modules/edit_media.py
+++ bot/modules/edit_media.py
@@ -31,7 +31,7 @@ async def edit_media(client, message):
         if forward_chat and not ignore_source:
             forward_chat_username = forward_chat.username
             forward_from_message_id = origin_message.forward_from_message_id
-            caption += f'\n💒SOURCE: <b><a href="https://t.me/{forward_chat_username}/{forward_from_message_id}">{forward_chat_username}</a></b>'
+            caption += f'\n🧿From: <b><a href="https://t.me/{forward_chat_username}/{forward_from_message_id}">{forward_chat_username}</a></b>'
         if origin_message.media_group_id:
             media_group = await client.get_media_group(chat_id, origin_message.id)
             send_medias = []
