5263a5e49b39856866dbc48270faab4079f46d4c
code0o0 <w17074919615@gmail.com>
2024-04-16 09:27:38 +0800

    Fix

--- bot/modules/forward_media.py
+++ bot/modules/forward_media.py
@@ -55,9 +55,9 @@ async def forward_message(client, message, message_id):
     except Exception as e:
         try:
             tgclient = user
-            if handler_dict[message_id]['forward_chat'] == OWNER_ID:
+            if from_chat == OWNER_ID:
                 bot_user = await client.get_me()
-                handler_dict[message_id]['forward_chat'] = bot_user.id
+                from_chat = bot_user.id
             message_list = await user.get_messages(from_chat, messages_id_list)
         except Exception as e:
             LOGGER.error(e) 
