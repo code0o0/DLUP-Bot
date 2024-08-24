af43e8f5932d24bea751b817d0e60bc1f356bfc5
code0o0 <w17074919615@gmail.com>
2024-08-24 08:21:31 +0800

    test

--- bot/modules/bot_settings.py
+++ bot/modules/bot_settings.py
@@ -357,8 +357,8 @@ async def edit_nzb(message, pre_message, key):
         value = ",".join(eval(value))
     res = await sabnzbd_client.set_config("misc", key, value)
     if res["config"]["misc"][key] != value:
-        await sabnzbd_client.restart()
-    nzb_options[key] = value
+        await sabnzbd_client.restart_repair()
+    nzb_options[key] = await sabnzbd_client.get_config("misc", key)
     await update_buttons(pre_message, "nzb")
     await deleteMessage(message)
     if DATABASE_URL:
