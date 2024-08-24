daf3027c2b2d78571cd57876de84f7c1c6a39ac3
code0o0 <w17074919615@gmail.com>
2024-08-24 13:01:22 +0800

    fix bug

--- bot/modules/bot_settings.py
+++ bot/modules/bot_settings.py
@@ -146,7 +146,7 @@ async def get_buttons(key=None, edit_type=None):
             msg += "<b>Timeout:</b> 60 sec"
         elif edit_type.startswith("nzbsevar"):
             index = 0 if key == "newser" else int(edit_type.replace("nzbsevar", ""))
-            buttons.ibutton("Back", f"botset nzbser{index}", position="footer")
+            buttons.ibutton("Back", f"botset nzbserver", position="footer")
             buttons.ibutton("Close", "botset close", position="footer")
             if key != "newser":
                 buttons.ibutton("Empty", f"botset emptyserkey {index} {key}")
@@ -207,10 +207,9 @@ async def get_buttons(key=None, edit_type=None):
     elif key.startswith("nzbser"):
         msg = ""
         index = int(key.replace("nzbser", ""))
-        if len(config_dict["USENET_SERVERS"]) > index:
-            content_dict = config_dict["USENET_SERVERS"][index]
-            buttons, msg = get_content_buttons(content_dict, f"nzbsevar{index}", "nzbser")
-            buttons.ibutton("Remove Server", f"botset remser {index}", position="body")
+        content_dict = config_dict["USENET_SERVERS"][index]
+        buttons, msg = get_content_buttons(content_dict, f"nzbsevar{index}", "nzbser")
+        buttons.ibutton("Remove Server", f"botset remser {index}", position="body")
         buttons.ibutton("Back", "botset nzbserver", position="footer")
         buttons.ibutton("Close", "botset close", position="footer")
     button = buttons.build_menu(2, 6, 2, 2)
